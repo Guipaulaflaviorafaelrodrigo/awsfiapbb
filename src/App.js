dc3abc269c2b187ab2a31b0ad8977fbb776d0fc0
Guipaulaflaviorafaelrodrigo <113221498+Guipaulaflaviorafaelrodrigo@users.noreply.github.com>
2022-09-09 23:54:51 -0300

    Trabalho e integrantes

--- src/App.js
+++ src/App.js
@@ -7,12 +7,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Integrantes</h1> 
-        <h3>344972 FLAVIO LIMA CONSTANCIO </h3>                   
-        <h3>344769 GUILHERME OLIVEIRA MARÇANEIRO ZILSE</h3>
-        <h3>344987 PAULA MOURA MATTOS</h3>
-        <h3>344789 RAFAEL PRADO DE ANDRADE</h3>
-        <h3>344781 RODRIGO MESSIAS BARROS OLIVEIRA SILVA</h3>
+        <h1>TRABALHO DE CLOUD ARCHITECTURE & DEVOPS - JOSÉ CASTILLO LEMA</h1> 
+        <h2>INTEGRANTES:</h2>
+        <p>344972 FLAVIO LIMA CONSTANCIO</p>                   
+        <p>344769 GUILHERME OLIVEIRA MARÇANEIRO ZILSE</p>
+        <p>344987 PAULA MOURA MATTOS</p>
+        <p>344789 RAFAEL PRADO DE ANDRADE</p>
+        <p>344781 RODRIGO MESSIAS BARROS OLIVEIRA SILVA</p>
       </header>
     </div>
   );
