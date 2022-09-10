55aa34b616e13ae32c2e565ab5fb6a055cc14ccb
Guipaulaflaviorafaelrodrigo <113221498+Guipaulaflaviorafaelrodrigo@users.noreply.github.com>
2022-09-09 23:37:26 -0300

    Integrantes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,17 +7,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Integrantes</h1> 
+        <h3>344972 FLAVIO LIMA CONSTANCIO </h3>                   
+        <h3>344769 GUILHERME OLIVEIRA MARÇANEIRO ZILSE</h3>
+        <h3>344987 PAULA MOURA MATTOS</h3>
+        <h3>344789 RAFAEL PRADO DE ANDRADE</h3>
+        <h3>344781 RODRIGO MESSIAS BARROS OLIVEIRA SILVA</h3>
       </header>
     </div>
   );
